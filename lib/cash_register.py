4e6df1dd4901f4186a42b8eae709d38f794a5c8b
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-02-02 23:06:08 +0300

    first

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -2,3 +2,36 @@
 
 class CashRegister:
   pass
+  def __init__(self, discount=0, total=0, items=[]):
+    pass
+    self.discount = discount
+    self._total = total
+    self._items = items
+  
+  def getTotal(self):
+    return self._total
+  
+  def setTotal(self, total):
+    self._total = total
+  
+  def getItems(self):
+    return self._items
+  
+  def setItems(self, items):
+    self._items = [items]
+  
+  total = property(getTotal, setTotal)
+  items = property(getItems, setItems)
+
+  def add_item(self, title, price, quantity=1):
+    pass
+    self._total = self._total + (price * quantity)
+    self.items.append(title)
+  
+  def apply_discount(self, discount=0.2):
+    pass
+    if int(self._total - (self._total * discount)) == 0:
+      print("There is no discount to apply.")
+    else:
+      self._total = int(self._total - (self._total * discount))
+      print(f"After the discount, the total comes to ${self.total}.")
