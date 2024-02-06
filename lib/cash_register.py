195d83cdb64ebdad4f63e05e7f3f0720c7000255
tiffmaina@gmail.com <tiffmaina@gmail.com>
2024-02-06 21:48:45 +0300

    71 complete

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -18,7 +18,7 @@ class CashRegister:
     return self._items
   
   def setItems(self, items):
-    self._items = [items]
+    self._items = items
   
   total = property(getTotal, setTotal)
   items = property(getItems, setItems)
@@ -26,7 +26,9 @@ class CashRegister:
   def add_item(self, title, price, quantity=1):
     pass
     self._total = self._total + (price * quantity)
-    self.items.append(title)
+    self._items.append(title)
+    if quantity > 1:
+      self._items.append(title)
   
   def apply_discount(self, discount=0.2):
     pass
